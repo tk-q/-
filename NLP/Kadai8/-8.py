1f82d4aac13443ad1d510ee79ed531439b0a5e0f
tk-q <103816066+tk-q@users.noreply.github.com>
2022-07-08 20:22:18 +0900

    Create -8.py

---
+++ NLP/Kadai8/-8.py
@@ -0,0 +1,44 @@
+co=0
+country=[]
+l2=""
+with open('/Users/takeidaichi/NLP/Kadai8-7-1.txt') as file_in:
+    for line in file_in:
+        if co<=1:
+            co+=1
+            continue
+        else:
+            k=0
+            for s in line.split(","):
+                k+=1
+                if k==3:
+                    break
+                if k==2:
+                    s=s.replace('"','')
+                    s=s.replace(" ","_")
+                    country.append(s)
+                    
+from gensim.models import KeyedVectors
+path="/Users/takeidaichi/NLP/GoogleNews-vectors-negative300.bin"
+model = KeyedVectors.load_word2vec_format(path, binary=True)
+print(type(model))
+
+X=[]
+Y=[]
+for C in country:
+    if C in model:
+        X.append(model[C])
+        Y.append(C)
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
+linkage_result = linkage(X, method='ward', metric='euclidean')
+plt.figure(num=None, figsize=(16, 9), dpi=200, facecolor='w', edgecolor='k')
+dendrogram(linkage_result, labels=Y)
+plt.show()
